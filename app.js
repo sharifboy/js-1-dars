b5e6207eaec17a8a8f5a4c48f8b31c8fdbbfba86
sharifboy <sharifboy@gmail.com>
2023-03-05 08:01:59 +0500

    made initial layouts

---
+++ app.js
@@ -0,0 +1,43 @@
+// //  variables
+
+// var test = "hello world";
+// var a = 10;
+// var b = 15;
+
+// console.log(test);
+// console.log(a + b);
+// let userAge = prompt("Yoshingizni kiriting");
+// confirm("are you sure exit this page");
+// prompt("Yoshingizni kiriting");
+// alert("Congratulation! You filled this form succesfully!");
+
+// if (userAge > 18) {
+//     alert("voyaga yetgan")
+// } else if (userAge < 18) {
+//     alert("voyaga yetmagan");
+// }
+
+let userBall = prompt("Balingizni kiriting")
+
+if (userBall > 0 && userBall < 56) {
+    alert(2)
+} else if (userBall > 55 && userBall < 71) {
+    alert(3)
+} else if (userBall > 72 && userBall < 86) {
+    alert(4)
+} else {
+    alert(5)
+}
+
+// let weight = prompt("vazningizni kiriting");
+// let heihght = prompt("Bo'yingizni kiriting");
+
+// if (weight / heihght * heihght > 18) {
+//     alert("under weight")
+// } else if (weight / heihght * heihght > 18 && weight / heihght < 24) {
+//     alert("normal")
+// } else if (weight / heihght * heihght < 25 && weight / heihght < 29) {
+//     alert("over weight")
+// } else {
+//     alert("obesity")
+// }
